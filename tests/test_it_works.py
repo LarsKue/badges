9bf92de8c033737af8a865f406bafb791a51585a
larskue <lars@kuehmichel.de>
2024-06-14 13:52:31 +0200

    xfail test

--- tests/test_it_works.py
+++ tests/test_it_works.py
@@ -1,4 +1,6 @@
 
+import pytest
+
 from badges import add_numbers
 
 
@@ -6,6 +8,7 @@ def test_add_numbers():
     assert add_numbers(2, 2) == 4
 
 
+@pytest.mark.xfail
 def test_fail():
     assert False
 
